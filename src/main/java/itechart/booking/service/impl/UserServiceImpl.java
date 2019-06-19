db52db0ea4fbf14b4c4c24875868d78093b6c193
Iryna Skiba <iryna.skiba.98@gmail.com>
2019-06-19 12:01:55 +0300

    added oauth2 + jwt

--- src/main/java/itechart/booking/service/impl/UserServiceImpl.java
+++ src/main/java/itechart/booking/service/impl/UserServiceImpl.java
@@ -1,8 +1,33 @@
 package itechart.booking.service.impl;
 
+import itechart.booking.model.User;
+import itechart.booking.repository.UserRepository;
 import itechart.booking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserServiceImpl implements UserService {
+import java.util.Arrays;
+import java.util.List;
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserDetailsService, UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login);
+        if(user == null){
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), getAuthority());
+    }
+
+    private List getAuthority() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
 }
